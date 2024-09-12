0730b6366508eb323538423ed78e775bf4b3ce7a
Blake Lapum <blake.lapum@spindance.com>
2024-09-12 08:57:58 -0400

    validated comms between front/backend apps on grpc - both localhost and local IP of computer

--- example/frontend/lib/main.dart
+++ example/frontend/lib/main.dart
@@ -1,33 +1,82 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:logging/logging.dart';
+import 'package:unleashing_grpc/unleashing_grpc.dart';
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((final LogRecord record) {
+    String start = '\x1b[0m';
+    const String reset = '\x1b[0m';
+
+    switch (record.level) {
+      case Level.FINEST:
+      case Level.FINER:
+        // Grey.
+        start = '\x1b[90m';
+      case Level.FINE:
+      case Level.CONFIG:
+        // White.
+        start = '\x1b[97m';
+      case Level.INFO:
+        // Green.
+        start = '\x1b[32m';
+      case Level.WARNING:
+        // Yellow.
+        start = '\x1b[33m';
+      case Level.SEVERE:
+        // Red.
+        start = '\x1b[31m';
+      case Level.SHOUT:
+        start = '\x1b[41m\x1b[93m';
+    }
+
+    stdout.writeln(
+      '$start(${record.time.toLocal()}) ${record.loggerName}(${record.level.name}): ${record.message}$reset',
+    );
+  });
+}
+
+void main() async {
+  _setupLogging();
+
+  // Connect to the gRPC server.
+  final Logger logger = Logger('main');
+  logger.info('Connecting to gRPC server');
+
+  final String hostname = Platform.environment['HOSTNAME'] ?? 'localhost';
+
+  final MachineControlGrpcClient grpcClient = MachineControlGrpcClient(
+    hostname: hostname,
+    logger: logger,
+  );
+
+  while (true) {
+    try {
+      await grpcClient.setLedState(true);
+      await Future<void>.delayed(const Duration(seconds: 1));
+      await grpcClient.setLedState(false);
+      break;
+    } catch (e, stackTrace) {
+      logger.severe('Error setting LED state: $e, $stackTrace');
+    }
+
+    await Future<void>.delayed(const Duration(seconds: 1));
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(final BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -37,16 +86,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
@@ -59,50 +99,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(final BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -119,7 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
